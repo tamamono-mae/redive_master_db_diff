25ee83cd4274fe2f1cd8985601775056b692f6a3
tamamo-s <79314960+tamamo-s@users.noreply.github.com>
2022-11-24 10:52:09 +0800

    10043200 - new unit: ★★★クリスティーナ - new event ハッピー・ハプニング　\n幸せの双子天使と聖夜の贈り物 - new gacha ★3キャラ出現率2倍！限定「キャル」「ヒヨリ」出現率UP！\n対象キャラを仲間にする度に対象メモピ200個GET！

--- story_character_mask.sql
+++ story_character_mask.sql
@@ -156,6 +156,7 @@ INSERT INTO `story_character_mask` VALUES (/*chara_id*/8611, /*offset*/0, /*size
 INSERT INTO `story_character_mask` VALUES (/*chara_id*/8711, /*offset*/0, /*size*/1000, /*softness*/50);
 INSERT INTO `story_character_mask` VALUES (/*chara_id*/8712, /*offset*/0, /*size*/1000, /*softness*/50);
 INSERT INTO `story_character_mask` VALUES (/*chara_id*/8811, /*offset*/0, /*size*/1000, /*softness*/50);
+INSERT INTO `story_character_mask` VALUES (/*chara_id*/8911, /*offset*/0, /*size*/800, /*softness*/50);
 INSERT INTO `story_character_mask` VALUES (/*chara_id*/11411, /*offset*/-15, /*size*/730, /*softness*/0);
 INSERT INTO `story_character_mask` VALUES (/*chara_id*/13311, /*offset*/-45, /*size*/715, /*softness*/60);
 INSERT INTO `story_character_mask` VALUES (/*chara_id*/15011, /*offset*/0, /*size*/2000, /*softness*/50);
