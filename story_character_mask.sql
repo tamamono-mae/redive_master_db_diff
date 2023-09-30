056931e82d81baa883207269ddaa925a894fa1cd
tamamo-s <79314960+tamamo-s@users.noreply.github.com>
2023-09-30 10:53:37 +0800

    10051000 - new unit: ★★★サレン（サラサリア） - new gacha 限定「サレン（サラサリア）」登場

--- story_character_mask.sql
+++ story_character_mask.sql
@@ -762,7 +762,9 @@ INSERT INTO `story_character_mask` VALUES (/*chara_id*/127213, /*offset*/-75, /*
 INSERT INTO `story_character_mask` VALUES (/*chara_id*/127214, /*offset*/-75, /*size*/700, /*softness*/55);
 INSERT INTO `story_character_mask` VALUES (/*chara_id*/127311, /*offset*/0, /*size*/700, /*softness*/65);
 INSERT INTO `story_character_mask` VALUES (/*chara_id*/127312, /*offset*/0, /*size*/700, /*softness*/65);
+INSERT INTO `story_character_mask` VALUES (/*chara_id*/127511, /*offset*/-45, /*size*/630, /*softness*/60);
 INSERT INTO `story_character_mask` VALUES (/*chara_id*/127512, /*offset*/-45, /*size*/630, /*softness*/60);
+INSERT INTO `story_character_mask` VALUES (/*chara_id*/127611, /*offset*/-35, /*size*/700, /*softness*/65);
 INSERT INTO `story_character_mask` VALUES (/*chara_id*/127612, /*offset*/-35, /*size*/700, /*softness*/65);
 INSERT INTO `story_character_mask` VALUES (/*chara_id*/161199, /*offset*/0, /*size*/800, /*softness*/50);
 INSERT INTO `story_character_mask` VALUES (/*chara_id*/161499, /*offset*/0, /*size*/800, /*softness*/50);
